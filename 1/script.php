4d2d4fb6077ebe810d1666b128e77c021b4b523b
Daniel Leech <dan.t.leech@gmail.com>
2021-12-03 00:13:37 +0100

    Day 1

---
+++ 1/script.php
@@ -0,0 +1,20 @@
+<?php
+
+echo array_reduce(
+    array_map(
+        'intval',
+        explode(
+            "\n",
+            file_get_contents(
+                __DIR__ . '/input'
+            )
+        )
+    ), 
+    static function (int $count, int $number) {
+        static $prev = PHP_INT_MAX;
+        $count += $number > $prev ? 1 : 0;
+        $prev = $number;
+        return $count;
+    },
+    0
+);
