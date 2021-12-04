101f78d37745ca55869a07ba3efa47d220169521
Daniel Leech <dan.t.leech@gmail.com>
2021-12-04 11:25:18 +0100

    Part II

--- 2/script.php
+++ 2/script.php
@@ -5,17 +5,24 @@ echo array_reduce(
         fn (string $in) => explode(' ', $in),
         explode("\n", trim(file_get_contents(__DIR__ . '/input')))
     ),
-    function (object $position, array $cmd): object {
-        match ($cmd[0]) {
-            'forward' => $position->position+=$cmd[1],
-            'down' => $position->depth+=$cmd[1],
-            'up' => $position->depth-=$cmd[1],
+    function (object $nav, array $cmd): object {
+        switch ($cmd[0]) {
+            case 'forward':
+                $nav->position+=$cmd[1];
+                $nav->depth += ($nav->aim * $cmd[1]);
+                break;
+            case 'down':
+                $nav->aim+=$cmd[1];
+                break;
+            case 'up':
+                $nav->aim-=$cmd[1];
         };
-        return $position;
+        return $nav;
     },
     new class() {
         public int $depth = 0;
         public int $position = 0;
+        public int $aim = 0;
         public function multiply(): int {
             return $this->depth * $this->position;
         }
