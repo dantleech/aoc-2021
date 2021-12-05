1ecbf9036ac498e520f83608a9cf37dbbecfafc8
Daniel Leech <dan.t.leech@gmail.com>
2021-12-05 16:59:18 +0100

    Part II

--- 5/script.php
+++ 5/script.php
@@ -8,41 +8,51 @@ class Seg {
     public function isStraight(): bool {
         return $this->start->x === $this->end->x || $this->start->y === $this->end->y;
     }
+    public function isDiagonal(): bool {
+        return abs($this->start->x - $this->end->x) === abs($this->start->y - $this->end->y);
+
+    }
 }
 class Diagram {
     private array $grid = [];
     public function plot(Seg $seg): void {
-        if (!$seg->isStraight()) {
-            return;
-        }
-        foreach (range($seg->start->x, $seg->end->x) as $x) {
-            if (!isset($this->grid[$x])) {
-                $this->grid[$x] = [];
-            }
-            foreach (range($seg->start->y, $seg->end->y) as $y) {
-                if (isset($this->grid[$x][$y])) {
-                    $this->grid[$x][$y]++;
-                    continue;
+        $xRange = range($seg->start->x, $seg->end->x);
+        $yRange = range($seg->start->y, $seg->end->y);
+        if ($seg->isStraight()) {
+            foreach ($xRange as $x) {
+                foreach ($yRange as $y) {
+                    $this->inc($x, $y);
                 }
-                $this->grid[$x][$y] = 1;
+            }
+        }
+        if ($seg->isDiagonal()) {
+            foreach (range($seg->start->x, $seg->end->x) as $i => $x) {
+                $y = $yRange[$i];
+                $this->inc($x, $y);
             }
         }
     }
-
-    public static function plotFromSegments(array $segments): Diagram
-    {
+    public static function plotFromSegments(array $segments): Diagram {
         return array_reduce($segments, function (Diagram $dia, Seg $seg) {
             $dia->plot($seg);
             return $dia;
         }, new Diagram());
     }
-
-    public function overlapingPoints(): int
-    {
+    public function overlapingPointCount(): int {
         return array_reduce($this->grid, function (int $count, array $yAxis) {
             $count += count(array_filter($yAxis, fn (int $count) => $count > 1));
             return $count;
         }, 0);
+    }
+    private function inc(int $x, int $y): void {
+        if (!isset($this->grid[$x])) {
+            $this->grid[$x] = [];
+        }
+        if (isset($this->grid[$x][$y])) {
+            $this->grid[$x][$y]++;
+            return;
+        }
+        $this->grid[$x][$y] = 1;
     }
 }
 
@@ -52,5 +62,4 @@ $diagram = Diagram::plotFromSegments(array_map(function (string $line) {
     }, explode(' -> ', $line)));
 }, explode("\n", trim(file_get_contents(__DIR__ . '/input')))));
 
-echo $diagram->overlapingPoints();
-
+echo $diagram->overlapingPointCount();
