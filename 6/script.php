31678042c968aaa3d1e5d360f2ec28c9c17a8ecf
Daniel Leech <dan.t.leech@gmail.com>
2021-12-06 19:54:56 +0100

    Brain fried

--- 6/script.php
+++ 6/script.php
@@ -2,16 +2,26 @@
 
 $lanterns = array_map('intval', explode(',', trim(file_get_contents(__DIR__ . '/input'))));
 
-for ($day = 1; $day <= 80; $day++) {
-    foreach ($lanterns as &$lantern) {
-        if ($lantern === 0) {
-            $lantern = 6;
-            $lanterns[] = 9;
-        } else {
-            $lantern--;
-        }
-    }
+$ageCounts = array_fill(0, 9, 0);
+foreach ($lanterns as $lantern) {
+    $ageCounts[$lantern]++;
 }
 
-echo count($lanterns);
+for ($x = 0; $x < 256; $x++) {
+    $gen = $ageCounts;
+    foreach ($ageCounts as $age => $count) {
+        if ($age === 0) {
+            $gen[0]-=$count;
+            $gen[6]+=$count;
+            $gen[8]+=$count;
+            continue;
+        }
+
+        $gen[$age]-=$count;
+        $gen[$age-1]+=$count;
+    }
+    $ageCounts = $gen;
+}
+
+echo array_sum($gen);
 
