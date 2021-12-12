fd689cae9c6e48ef475e0e3e7dcfc7c925288539
Daniel Leech <dan.t.leech@gmail.com>
2021-12-13 00:09:08 +0100

    Part 1 awful

---
+++ 11/script.php
@@ -0,0 +1,109 @@
+<?php
+
+$grid = new Grid(array_map(
+    fn (string $line) => str_split($line),
+    explode("\n", trim(
+        file_get_contents(__DIR__ . '/input')
+    ))
+));
+
+class Grid {
+    public int $nbFlashes = 0;
+
+    public function __construct(public array $grid) {
+    }
+
+    /**
+     * - Increase all cells by 1
+     * - 10: For each cell now > 9
+     *   - has it flashed? 
+     *     - increase all surrounding squares by 1
+     *   - set cell to 0
+     *   - add cell to flashed list
+     * - are there any squares > 9?
+     *   - goto 10
+     */
+    public function increment(): self {
+        $grid = $this->map(fn (int $level) => $level + 1);
+        $grid->flash();
+
+        return $grid;
+    }
+
+    private function flash(array $flashed = []): self {
+        $didFlash = false;
+        foreach ($this->grid as $y => $octopi) {
+            foreach ($octopi as $x => $energy) {
+                $hash = $this->hash($x, $y);
+
+                if ($energy <= 9) {
+                    continue;
+                }
+
+                $this->grid[$y][$x] = 0;
+
+                if (isset($flashed[$hash])) {
+                    continue;
+                }
+
+                $flashed[$hash] = true;
+                $didFlash = true;
+                $this->nbFlashes++;
+
+                // increase adjacent squares
+                foreach ([
+                    [0, -1], [ 1, 1], [ 1, -1], [ 1, 0],
+                    [0,  1], [-1, 1], [-1, -1], [-1, 0]
+                ] as [$xo, $yo]) {
+                    $this->apply($flashed, $x + $xo, $y + $yo, fn (int $energy) => $energy + 1);
+                }
+            }
+        }
+
+        if ($didFlash) {
+            $this->flash($flashed);
+        }
+
+        return $this;
+    }
+
+    public function apply(array $flashed, int $x, int $y, Closure $closure): void {
+        if (isset($flashed[$this->hash($x, $y)])) {
+            return;
+        }
+        if (!isset($this->grid[$y][$x])) {
+            return;
+        }
+        $this->grid[$y][$x] = $closure($this->grid[$y][$x]);
+    }
+
+    public function map(Closure $closure): self {
+        $ng = $this->grid;
+        foreach ($this->grid as $y => $octopi) {
+            foreach ($octopi as $x => $octopus) {
+                $ng[$y][$x] = $closure($ng[$y][$x]);
+            }
+        }
+        return new self($ng);
+    }
+
+    public function toString(): string {
+        return implode("\n", array_map(fn (array $chars) => implode(' ', $chars), $this->grid));
+    }
+
+    private function hash($x, $y): string
+    {
+        $hash = $x.'.'.$y;
+        return $hash;
+    }
+}
+
+$nbFlashes = 0;
+for ($s = 0; $s < 100; $s++) {
+    $grid = $grid->increment();
+    $nbFlashes+= $grid->nbFlashes;
+    echo $grid->toString().PHP_EOL.PHP_EOL;
+}
+
+echo $nbFlashes . PHP_EOL;
+
