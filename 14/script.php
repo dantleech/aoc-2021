ff52fd19d22203ea3f2d66ee0a927c9d4c6c5606
Daniel Leech <dan.t.leech@gmail.com>
2021-12-26 17:56:28 +0000

    Shame

--- 14/script.php
+++ 14/script.php
@@ -1,44 +1,92 @@
 <?php
 
-[$template, $rules] = (fn (array $p) => [
-    $p[0],
+class Pair {
+    public function __construct(public string $left, public string $right) {}
+    public function __toString(): string { return $this->left.$this->right; }
+}
+
+class Rule {
+    public function __construct(public string $pair, public string $insert) {}
+}
+
+class Counter {
+    public function __construct(public array $counts = []) {}
+    public function inc(Pair $pair, int $amount = 1) {
+        if (!isset($this->counts[(string)$pair])) {
+            $this->counts[(string)$pair] = 0;
+        }
+        $this->counts[(string)$pair]+= $amount;
+    }
+    public function pairs(): array {
+        return array_map(fn (string $pair) => new Pair(...str_split($pair)), array_keys($this->counts));
+    }
+    public function count(Pair $pair): int {
+        return $this->counts[(string)$pair];
+    }
+    public function charCounts(string $firstChar): array {
+        $counts = [
+            $firstChar => 1
+        ];
+        foreach ($this->pairs() as $i => $pair) {
+            if (!isset($counts[$pair->left])) {
+                $counts[$pair->left] = 0;
+            }
+            $counts[$pair->left]+= $this->count($pair);
+        }
+        var_dump($counts);
+
+        return $counts;
+    }
+
+    public function max(): int {
+        return max($this->counts);
+    }
+    public function min(): int {
+        return min($this->counts);
+    }
+
+    public function sum(): int {
+        return array_sum($this->counts);
+    }
+}
+
+[$chars, $rules] = (fn (array $p) => [
+    str_split($p[0]),
     (fn (array $rules) => array_combine(array_map(fn (Rule $r) => $r->pair, $rules), $rules))(
         array_map(
             fn (string $rule) => new Rule(...explode(' -> ', $rule)),
             explode("\n", $p[1])
         )
     )
-])(explode("\n\n", trim(file_get_contents(__DIR__ .'/testinput'))));
+])(explode("\n\n", trim(file_get_contents(__DIR__ .'/input'))));
 
-class Rule {
-    public function __construct(public string $pair, public string $insert) {}
+$pairs = [];
+for ($i = 0; $i < count($chars) - 1; $i++) {
+    $pairs[] = new Pair($chars[$i], $chars[$i+1]);
 }
 
-$chars = str_split($template);
+$counter = new Counter([]);
 
-$newChars = [];
-$lastChar = null;
+foreach ($pairs as $i => $pair) {
+    $counter->inc($pair);
+}
 
-for ($i = 0; $i < 10; $i++) {
-    foreach ($chars as $char) {
-        if (empty($newChars)) {
-            $newChars = [$char];
-            continue;
-        }
+for ($i = 0; $i < 40; $i++) {
+    $newCounter = new Counter();
+    foreach ($counter->pairs() as $pair) {
+        $insert = $rules[(string)$pair]->insert;
 
-        $key = array_slice($newChars, -1)[0].$char;
-        $rule = $rules[$key];
-        $newChars[] = $rule->insert;
-        $newChars[] = $char;
+        $newCounter->inc(
+            new Pair($pair->left, $insert),
+            $counter->count($pair)
+        );
+
+        $newCounter->inc(
+            new Pair($insert, $pair->right),
+            $counter->count($pair)
+        );
     }
-    $chars = $newChars;
-    $newChars = [];
+    $counter = $newCounter;
 }
-$counts = [];
-foreach ($chars as $char) {
-    if (!isset($counts[$char])) {
-        $counts[$char] = 0;
-    }
-    $counts[$char]++;
-}
-echo max($counts) - min($counts);
+$firstChar = $pairs[array_key_last($pairs)]->right;
+var_dump(max($counter->charCounts($firstChar)) - min($counter->charCounts($firstChar)));
